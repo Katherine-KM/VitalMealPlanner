cc3ffdcf1e62836faf1a60a970bd523a37050fc4
Katherine Marcott <katherinesoftwareengineer@gmail.com>
2022-10-04 11:24:21 -0400

    add user image to userschema

--- models/User.js
+++ models/User.js
@@ -5,6 +5,10 @@ const UserSchema = new mongoose.Schema({
   userName: { type: String, unique: true },
   email: { type: String, unique: true },
   password: String,
+  image: {
+    type: String,
+    require: true,
+  },
 });
 
 // Password hash middleware.
