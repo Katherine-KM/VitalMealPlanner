b765b7395e19443becb4f03d5ef62ff357622e9e
Katherine Marcott <katherinesoftwareengineer@gmail.com>
2022-11-05 19:05:43 -0400

    add styling to login/signup page and add picture for login/signup page

--- public/js/darkmode.js
+++ public/js/darkmode.js
@@ -1,16 +1,19 @@
 let darkMode = localStorage.getItem('darkMode');
 let headerElement = document.getElementById('header');
+let loginContainer = document.querySelector('.main-login-container');
 const darkModeToggle = document.querySelector('#dark-mode-toggle');
 
 const enableDarkMode = () => {
     document.body.classList.add('darkmode');
-    headerElement.classList.add('darkmode-header'); 
+    headerElement.classList.add('darkmode-dark'); 
+    loginContainer.classList.add('darkmode-dark');
     localStorage.setItem('darkMode', 'enabled');
 }
 
 const disableDarkMode = () => {
     document.body.classList.remove('darkmode');
-    headerElement.classList.remove('darkmode-header'); 
+    headerElement.classList.remove('darkmode-dark'); 
+    loginContainer.classList.remove('darkmode-dark');
     localStorage.setItem('darkMode', null);
 }
 
