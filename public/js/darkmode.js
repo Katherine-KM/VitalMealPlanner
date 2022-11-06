851b03af8b8f99e211962459c766935e4a1608c2
Katherine Marcott <katherinesoftwareengineer@gmail.com>
2022-11-06 11:11:31 -0500

    add styling to login page and change moon to sun in darkmode

--- public/js/darkmode.js
+++ public/js/darkmode.js
@@ -8,12 +8,16 @@ const enableDarkMode = () => {
     headerElement.classList.add('darkmode-dark'); 
     loginContainer.classList.add('darkmode-dark');
     localStorage.setItem('darkMode', 'enabled');
+    darkModeToggle.classList.remove('fa-moon');
+    darkModeToggle.classList.add('fa-sun');
 }
 
 const disableDarkMode = () => {
     document.body.classList.remove('darkmode');
     headerElement.classList.remove('darkmode-dark'); 
     loginContainer.classList.remove('darkmode-dark');
+    darkModeToggle.classList.add('fa-moon');
+    darkModeToggle.classList.remove('fa-sun');
     localStorage.setItem('darkMode', null);
 }
 
