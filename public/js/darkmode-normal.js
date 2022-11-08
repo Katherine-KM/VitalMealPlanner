c22a44d1126f80bf975f183a7bec479d862352fc
Katherine Marcott <katherinesoftwareengineer@gmail.com>
2022-11-08 13:21:06 -0500

    fix darkmode by creating new script

--- public/js/darkmode-normal.js
+++ public/js/darkmode-normal.js
@@ -1,21 +1,19 @@
 let darkMode = localStorage.getItem('darkMode');
 let headerElement = document.getElementById('header');
-let loginContainer = document.querySelector('.main-login-container');
 const darkModeToggle = document.querySelector('#dark-mode-toggle');
 
 const enableDarkMode = () => {
     document.body.classList.add('darkmode');
     headerElement.classList.add('darkmode-dark'); 
-    loginContainer.classList.add('darkmode-dark');
     localStorage.setItem('darkMode', 'enabled');
     darkModeToggle.classList.remove('fa-moon');
     darkModeToggle.classList.add('fa-sun');
+    console.log('test')
 }
 
 const disableDarkMode = () => {
     document.body.classList.remove('darkmode');
     headerElement.classList.remove('darkmode-dark'); 
-    loginContainer.classList.remove('darkmode-dark');
     darkModeToggle.classList.add('fa-moon');
     darkModeToggle.classList.remove('fa-sun');
     localStorage.setItem('darkMode', null);
