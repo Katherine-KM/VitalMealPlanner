478879cec95420f3536ffadacb7b9956122422f1
Katherine Marcott <katherinesoftwareengineer@gmail.com>
2022-11-11 17:20:08 -0500

    darkmode change

--- public/js/darkmode-normal.js
+++ public/js/darkmode-normal.js
@@ -1,10 +1,15 @@
 let darkMode = localStorage.getItem('darkMode');
 let headerElement = document.getElementById('header');
+let dmDarkContainer = document.getElementsByClassName('dm-dark-container');
+let dmContainer = document.getElementsByClassName('dm-container');
 const darkModeToggle = document.querySelector('#dark-mode-toggle');
 
 const enableDarkMode = () => {
     document.body.classList.add('darkmode');
-    headerElement.classList.add('darkmode-dark'); 
+    headerElement.classList.add('darkmode-dark');
+    [].forEach.call(dmContainer, function(mySingleDiv) {
+        mySingleDiv.classList.add('darkmode-dark');
+    }); 
     localStorage.setItem('darkMode', 'enabled');
     darkModeToggle.classList.remove('fa-moon');
     darkModeToggle.classList.add('fa-sun');
@@ -14,6 +19,9 @@ const enableDarkMode = () => {
 const disableDarkMode = () => {
     document.body.classList.remove('darkmode');
     headerElement.classList.remove('darkmode-dark');
+    [].forEach.call(dmContainer, function(mySingleDiv) {
+        mySingleDiv.classList.remove('darkmode-dark');
+    }); 
     darkModeToggle.classList.add('fa-moon');
     darkModeToggle.classList.remove('fa-sun');
     localStorage.setItem('darkMode', null);
