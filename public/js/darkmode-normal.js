e60ef9a9d7ef7a011e201ce9090ecedc11eb4944
Katherine Marcott <katherinesoftwareengineer@gmail.com>
2022-11-11 17:19:01 -0500

    began creating consistency in darkmode file

--- public/js/darkmode-normal.js
+++ public/js/darkmode-normal.js
@@ -1,10 +1,14 @@
 let darkMode = localStorage.getItem('darkMode');
 let headerElement = document.getElementById('header');
+let dmContainer = document.getElementsByClassName('dm-container');
 const darkModeToggle = document.querySelector('#dark-mode-toggle');
 
 const enableDarkMode = () => {
     document.body.classList.add('darkmode');
-    headerElement.classList.add('darkmode-dark'); 
+    headerElement.classList.add('darkmode-dark');
+    [].forEach.call(dmContainer, function(mySingleDiv) {
+        mySingleDiv.classList.add('darkmode-dark');
+    }); 
     localStorage.setItem('darkMode', 'enabled');
     darkModeToggle.classList.remove('fa-moon');
     darkModeToggle.classList.add('fa-sun');
@@ -14,6 +18,9 @@ const enableDarkMode = () => {
 const disableDarkMode = () => {
     document.body.classList.remove('darkmode');
     headerElement.classList.remove('darkmode-dark');
+    [].forEach.call(dmContainer, function(mySingleDiv) {
+        mySingleDiv.classList.remove('darkmode-dark');
+    }); 
     darkModeToggle.classList.add('fa-moon');
     darkModeToggle.classList.remove('fa-sun');
     localStorage.setItem('darkMode', null);
